6f0b991b989fc5e5233c4c7bf47a916515e9821e
V5788 (Course ID) <v5788@cam.ac.uk>
2015-08-14 12:27:01 +0100

    first attempt flying the drone

--- drone-backend.js
+++ drone-backend.js
@@ -11,11 +11,54 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
-    .on("ready", fly);
-    
+    .device("nav", {
+driver: "ardrone-nav",      // Combine with a second device to have more information
+    connection: "ardrone"
+})
+.on("ready", fly);
+
+var bot;
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    // Only retrieve a limited amount of navigation data
+    // As recommended by Parrot AR Drone developer's guide
+    bot.drone.config('general:navdata_demo', 'TRUE');
 
+    bot.nav.on("navdata", function(data) {
+        // console.log(data);
+    });
+
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        // Drone is higher than 1.5 meters up
+        if (altitude > 1.5) {
+            bot.drone.land();
+        }
+    });
+
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+    });
+
+    // Disable emergency setting if there was any
+    bot.drone.disableEmergency();
+    // Tell the drone it is lying horizontally
+    bot.drone.ftrim();
+
+    // Take off
+    bot.drone.takeoff();
+
+    after(2*1000, function() {
+        bot.drone.left(0.2);
+    });
+
+    after(10*1000, function() {
+        bot.drone.land();
+    });
+    after(15*1000, function() {
+        bot.drone.stop();
+    });
 }
 
 Cylon.start();
